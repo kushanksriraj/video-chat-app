730de36f370cb296b403212a65b72e208a31b32e
Kushank Sriraj <kushanksriraj@gmail.com>
2023-08-03 12:58:05 +0530

    foxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import App from "./App";
 import { createRoot } from "react-dom/client";
 
+// wake up the repl
+fetch("https://translater.kushanksriraj.repl.co/health").catch((err) =>
+  alert(err)
+);
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
